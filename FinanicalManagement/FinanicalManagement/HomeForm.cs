e12ee305354fc72e4b834067ef234a9d9f082d36
longdt <longdt555@gmail.com>
2020-09-25 18:15:46 +0700

    Update

--- FinanicalManagement/FinanicalManagement/HomeForm.cs
+++ FinanicalManagement/FinanicalManagement/HomeForm.cs
@@ -16,5 +16,22 @@ namespace FinanicalManagement
         {
             InitializeComponent();
         }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            this.Hide();
+            loginForm.ShowDialog();
+            this.Show();
+            Dispose();
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có thật sự muốn thoát chương trình?", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+
     }
 }
